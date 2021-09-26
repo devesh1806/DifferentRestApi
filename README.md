6e9f9ecb38ef5ab9cb87f979f46f972b1360f765
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-09-26 10:55:47 +0530

    Added basic setup

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>Different Rest API Calls</h1>
---
+++ src/test/java/com/bridgelabz/differentrestapi/DifferentRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.differentrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DifferentRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
