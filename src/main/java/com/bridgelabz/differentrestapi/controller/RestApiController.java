5db43707d768fb202528d1b089b4045968c601ad
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-09-26 11:35:42 +0530

    Added ability to make rest call and pass name as query parameter

--- src/main/java/com/bridgelabz/differentrestapi/controller/RestApiController.java
+++ src/main/java/com/bridgelabz/differentrestapi/controller/RestApiController.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.differentrestapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class RestApiController {
 	@RequestMapping(value = {"/",""})
 	public String hello() {
 		return "Hello From Bridgelabz";
+	}
+	
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String hello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + "!";
 	}
 }
