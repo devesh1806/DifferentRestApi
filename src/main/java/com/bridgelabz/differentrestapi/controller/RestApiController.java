fd2861ef946062a68219ffcef8558153b69be90f
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-09-26 12:03:33 +0530

    Added ability to rest call and pass firstname and lastname

--- src/main/java/com/bridgelabz/differentrestapi/controller/RestApiController.java
+++ src/main/java/com/bridgelabz/differentrestapi/controller/RestApiController.java
@@ -2,10 +2,14 @@ package com.bridgelabz.differentrestapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.differentrestapi.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -25,4 +29,11 @@ public class RestApiController {
 	public String helloParam(@PathVariable String name) {
 		return "Hello " + name + "!";
 	}
+	
+	@PostMapping("/post")
+	public String hello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName()+"!";
+	}
+	
+	
 }
